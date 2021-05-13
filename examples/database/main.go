6f59b92ed034392f8f77db4fbff43518a8939428
charly3pins <charlyfb90@gmail.com>
2021-05-13 23:27:25 +0200

    example using the get database endpoint

---
+++ examples/database/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/charly3pins/notion"
+)
+
+const (
+	baseURL       = "https://api.notion.com"
+	apiVersion    = "v1"
+	headerVersion = "2021-05-13"
+)
+
+func main() {
+	token := os.Getenv("ACCESS_TOKEN")
+	if token == "" {
+		log.Fatal("ACCESS_TOKEN env var missing")
+	}
+	dbID := os.Getenv("DB_ID")
+	if dbID == "" {
+		log.Fatal("DB_ID env var missing")
+	}
+	cli := notion.Client{
+		Config: notion.ClientConfig{
+			BaseURL:       baseURL,
+			APIVersion:    apiVersion,
+			HeaderVersion: headerVersion,
+			Token:         token,
+		},
+		Client: http.Client{
+			Timeout: 20 * time.Second,
+		},
+	}
+	resp, err := cli.GetDatabase(dbID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	enc, err := json.MarshalIndent(resp, "  ", "   ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(enc))
+}
